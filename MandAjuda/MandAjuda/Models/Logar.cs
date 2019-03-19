1ed59ac3ed0c9856108565ed51e55fa96b008a2d
Magal Junior <magaljunior@outlook.com>
2019-03-19 20:56:33 -0300

    Gerando Login

---
+++ MandAjuda/MandAjuda/Models/Logar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MandAjuda.Models
+{
+	public class Logar
+	{
+		
+		public int Codigo { get; set; }
+
+		public string Email { get; set; }
+
+		public string Senha { get; set; }
+
+	}
+}
---
+++ MandAjuda/MandAjuda/Repositories/Funcoes.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Web;
+using System.Web.Security;
+using MandAjuda.Models;
+
+
+namespace MandAjuda.Repositories
+{
+	public class Funcoes
+	{
+		public static bool AutenticarUsuario(string login, string senha)
+		{
+			Context _db = new Context();
+			var query = (from u in _db.Clientes where u.Email == login && u.Senha == senha select u).SingleOrDefault();
+			if (query == null)
+			{
+				return false;
+			}
+			FormsAuthentication.SetAuthCookie(query.Email, false);
+			HttpContext.Current.Session["Usuario"] = query.Email;
+			return true;
+		}
+		public static Cliente Getusuario()
+		{
+			string _login = HttpContext.Current.User.Identity.Name;
+			if (HttpContext.Current.Session.Count > 0 || HttpContext.Current.Session["Usuario"] != null)
+			{
+				_login = HttpContext.Current.Session["Usuario"].ToString();
+				if (_login == "")
+				{
+					return null;
+				}
+				else
+				{
+					Context _db = new Context();
+					Cliente cliente = (from u in _db.Clientes where u.Email == _login select u).SingleOrDefault();
+					return cliente;
+				}
+			}
+			else
+			{
+				return null;
+			}
+		}
+		public static Cliente GetUsuario(string _login)
+		{ 		
+			if (_login == "")
+			{
+				return null;
+			}
+			else
+			{
+				Context _db = new Context();
+				Cliente cliente = (from u in _db.Clientes where u.Email == _login select u).SingleOrDefault();
+
+				return cliente;
+			}
+		}
+		public static void Deslogar()
+		{
+			HttpContext.Current.Session["Usuario"] = "";
+			FormsAuthentication.SignOut();
+		}
+		
+	}
+}
