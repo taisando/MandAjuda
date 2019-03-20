772dc446ea94f429a9e986058fde7d91387f566d
Magal Junior <magaljunior@outlook.com>
2019-03-19 21:11:44 -0300

    Continuação do Login

--- MandAjuda/MandAjuda/Controllers/PublicoController.cs
+++ MandAjuda/MandAjuda/Controllers/PublicoController.cs
@@ -34,6 +34,28 @@ namespace MandAjuda.Controllers
 			MandAjuda.Repositories.Funcoes.Deslogar();
 			return RedirectToAction("logar", "Publico");
 		}
-		
-    }
+		[HttpPost]
+		public ActionResult Logar(string email, string senha)
+		{
+			if (Funcoes.AutenticarProfissional(email, senha) == false)
+			{
+				ViewBag.Error = "Usuário e/ou Senha Inválida";
+				return View();
+			}
+			return RedirectToAction("Index", "Home");
+		}
+		public ActionResult AcessoNegado()
+		{
+			using (Context c = new Context())
+			{
+				return View();
+			}
+		}
+		public ActionResult Logoff()
+		{
+			MandAjuda.Repositories.Funcoes.Deslogar();
+			return RedirectToAction("logar", "Publico");
+		}
+
+	}
 }
--- MandAjuda/MandAjuda/Repositories/Funcoes.cs
+++ MandAjuda/MandAjuda/Repositories/Funcoes.cs
@@ -59,11 +59,60 @@ namespace MandAjuda.Repositories
 				return cliente;
 			}
 		}
+		
+		public static bool AutenticarProfissional(string login, string senha)
+		{
+			Context _db = new Context();
+			var query = (from u in _db.Profissionais where u.Email == login && u.Senha == senha select u).SingleOrDefault();
+			if (query == null)
+			{
+				return false;
+			}
+			FormsAuthentication.SetAuthCookie(query.Email, false);
+			HttpContext.Current.Session["Usuario"] = query.Email;
+			return true;
+		}
+		public static Profissionais GetProfissional()
+		{
+			string _login = HttpContext.Current.User.Identity.Name;
+			if (HttpContext.Current.Session.Count > 0 || HttpContext.Current.Session["Usuario"] != null)
+			{
+				_login = HttpContext.Current.Session["Usuario"].ToString();
+				if (_login == "")
+				{
+					return null;
+				}
+				else
+				{
+					Context _db = new Context();
+					Profissional profissional = (from u in _db.Profissionais where u.Email == _login select u).SingleOrDefault();
+					return profissional;
+				}
+			}
+			else
+			{
+				return null;
+			}
+		}
+		public static Profissional GetProfissional(string _login)
+		{
+			if (_login == "")
+			{
+				return null;
+			}
+			else
+			{
+				Context _db = new Context();
+				Profissional profissional = (from u in _db.Profissionais where u.Email == _login select u).SingleOrDefault();
+
+				return profissional;
+			}
+		}
 		public static void Deslogar()
 		{
 			HttpContext.Current.Session["Usuario"] = "";
 			FormsAuthentication.SignOut();
 		}
-		
+
 	}
 }
